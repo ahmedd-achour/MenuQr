180357105d8e93aef77b74a4cdc62b5c7cffbad7
alaaSouayah <aladinesouayah@gmail.com>
2026-07-13 17:44:56 +0100

    update project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
 
 import { Firestore, doc, setDoc } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomepageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,6 @@
 import { Routes } from '@angular/router';
-import { PartnersMenuComponent } from './partners-menu/partners-menu.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 export const routes: Routes = [
-
-  {
-    path: 'partners-menu',
-    component: PartnersMenuComponent
-  },
-
- {
-    path: '**',
-    redirectTo: 'partners-menu'
-  }
+    { path: "", component: HomepageComponent }
 ];
