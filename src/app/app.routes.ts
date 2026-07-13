1faff83dfb909af6533547f13c1b61a9420914d2
alaaSouayah <aladinesouayah@gmail.com>
2026-07-13 18:05:18 +0100

    install firebase and update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,20 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { PartnersMenuComponent } from './partners-menu/partners-menu.component';
 
 export const routes: Routes = [
-    { path: "", component: HomepageComponent }
+    {
+        path: '',
+        component: PartnersMenuComponent
+    },
+
+    {
+        path: 'home',
+        component: HomepageComponent
+    },
+
+    {
+        path: '**',
+        redirectTo: 'home'
+    }
 ];
